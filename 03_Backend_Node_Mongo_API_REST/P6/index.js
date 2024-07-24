f2b7df3b491fff8a9649c834e00c59060627a397
Iker <iker.vera@gmail.com>
2024-07-24 12:59:35 +0200

    fixed misplaced files

--- 03_Backend_Node_Mongo_API_REST/P6/index.js
+++ 03_Backend_Node_Mongo_API_REST/P6/index.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const { connectDB } = require("./config/db");
-const juegosRouter = require("./api/routes/juegos");
-const plataformasRouter = require("./api/routes/plataformas");
-const cors = require("cors");
+const { connectDB } = require("./src/config/db");
+const juegosRouter = require("./src/api/routes/juegos");
+const plataformasRouter = require("./src/api/routes/plataformas");
+// const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 connectDB();
 
