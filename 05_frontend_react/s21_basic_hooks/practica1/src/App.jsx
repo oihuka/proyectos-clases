fa2aa06c315e1d16cb27137a5884f160ade6669a
Iker <iker.vera@gmail.com>
2024-09-13 17:16:29 +0200

    Initial commit

---
+++ 05_frontend_react/s21_basic_hooks/practica1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Listado from './components/Listado/Listado'
+import RickYMorty from './components/RickYMorty/RickYMorty'
+
+const App = () => {
+  return (
+    <div>
+      <RickYMorty />
+    </div>
+  )
+}
+
+export default App
---
+++ 05_frontend_react/s21_basic_hooks/practica1/src/components/Loading/Loading.jsx
@@ -0,0 +1,7 @@
+import './Loading.css'
+
+const Loading = () => {
+  return <img src='/assets/loading.gif' className='loading' />
+}
+
+export default Loading
